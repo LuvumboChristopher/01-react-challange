670fe4cd67f6622f917d4cc525c444caece1870b
LuvumboChristopher <christopherluvumbo.dev@gmail.com>
2022-02-15 17:12:41 +0100

    2 files modified

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const Contact = (props)=> {
-  return(
-    <div>
-      <div>
-        <h3>{props.name}</h3>
-        <h4>📞 {props.phone}</h4>
-        <h4>{props.email}</h4>
-        <button>Delete</button>
+const Contact = (props) => {
+  const isDeletable = props.isDeletable;
+
+  console.log(isDeletable);
+
+  return (
+    <div class='contact-info'>
+      <div class='frind-item'>
+        <h3>👨{props.name}</h3>
+        <h5>📞 {props.phone}</h5>
+        <h5>{props.email}</h5>
+        {props.isDeletable ? <button class='friend-item button delete-btn'>Delete</button> : null}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export  default Contact;
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ export const App = (
     <h1 className="text-center">My contacts list 📱</h1>
 
     <div>
-      <Contact name='Goku' email='goku@gmail.com' phone='075212354' />
-      <Contact name='Vegeta' email='vegeta@gmail.com' phone='075214532' />
-      <Contact name='Gohan' email='gohan@gmail.com' phone='075212353' />
-      <Contact name='Goten' email='goten@gmail.com' phone='078512356'/>
-      <Contact name='Picolo' email='picolo@gmail.com' phone='075369357' />
+      <Contact name='Goku' email='goku@gmail.com' phone='075212354' isDeletable={true} />
+      <Contact name='Vegeta' email='vegeta@gmail.com' phone='075214532' isDeletable={true}/>
+      <Contact name='Gohan' email='gohan@gmail.com' phone='075212353' isDeletable={false} />
+      <Contact name='Goten' email='goten@gmail.com' phone='078512356' isDeletable={true}/>
+      <Contact name='Picolo' email='picolo@gmail.com' phone='075369357' isDeletable={false} />
     </div>
   </div>
 );
