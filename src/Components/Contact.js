7c9e799423e39fe43e64c527d85e94ce78ccdbef
LuvumboChristopher <christopherluvumbo.dev@gmail.com>
2022-02-15 15:06:36 +0100

    1 file modified, 1 added

---
+++ src/Components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = ()=> {
+  return(
+    <div>
+      <div>
+        <h3>Vegeta</h3>
+        <h4>📞 03.49.79.52.46</h4>
+        <button>Delete</button>
+      </div>
+    </div>
+  )
+}
+
+export  default Contact;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import Contact from './Components/Contact.js';
 
 export const App = (
   // write your React Code here
@@ -9,31 +10,11 @@ export const App = (
     <h1 className="text-center">My contacts list 📱</h1>
 
     <div>
-      <div>
-        <h3>Vegeta</h3>
-        <h4>📞 03.49.79.52.46</h4>
-        <button>Delete</button>
-      </div>
-      <div>
-        <h3>Goku</h3>
-        <h4>📞 02.56.38.50.10</h4>
-        <button>Delete</button>
-      </div>
-      <div>
-        <h3>Picolo</h3>
-        <h4>📞 04.89.73.92.89</h4>
-        <button>Delete</button>
-      </div>
-      <div>
-        <h3>Goten</h3>
-        <h4>📞 02.82.47.55.96</h4>
-        <button>Delete</button>
-      </div>
-      <div>
-        <h3>Trunks</h3>
-        <h4>📞 02.82.47.55.96</h4>
-        <button>Delete</button>
-      </div>
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
     </div>
   </div>
 );
